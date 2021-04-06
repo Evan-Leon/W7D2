f3f69d41c03c2398b6d01fb543b3cbbfa4d685f8
Evan-Leon <evan.leon.j@gmail.com>
2021-04-06 16:56:22 -0400

    Finished functionality for creating new user, logging in and logging out

--- band_app/app/controllers/sessions_controller.rb
+++ band_app/app/controllers/sessions_controller.rb
@@ -5,8 +5,9 @@ class SessionsController < ApplicationController
     end
 
     def create 
+        
         user = User.find_by_credentials(params[:user][:email], params[:user][:password])
-
+         
         if user 
           login!(user)
           redirect_to user_url(user)
