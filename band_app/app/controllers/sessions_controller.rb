961ef22f95a599ee7ab3e8a58892308138ac3ff8
Evan-Leon <evan.leon.j@gmail.com>
2021-04-06 15:03:49 -0400

    Added necessary logic, methods, controllers, routes to make and log in new user

--- band_app/app/controllers/sessions_controller.rb
+++ band_app/app/controllers/sessions_controller.rb
@@ -3,4 +3,23 @@ class SessionsController < ApplicationController
     def new 
         render :new 
     end
+
+    def create 
+        user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if user 
+          login!(user)
+          redirect_to user_url(user)
+        else  
+          flash.now[:errors] = ["invalid credentials"]
+          render :new 
+        end
+    end
+
+    def destroy
+      logout!
+      flash[:success] = ["logged out successfully"]
+      redirect_to new_session_url
+    end
+
 end
--- band_app/app/controllers/users_controller.rb
+++ band_app/app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
 
 
+    def show 
+      @user = User.find(params[:id])
+
+      render :show 
+    end
+
     def new 
         @user = User.new 
 
@@ -8,7 +14,20 @@ class UsersController < ApplicationController
     end
 
     def create 
+        @user = User.new(user_params)
 
+        if @user.save 
+          login!(@user)
+          redirect_to user_url(@user)
+        else  
+          flash.now[:errors] = @user.errors.full_message
+          render :new
+        end
+    end
+
+
+    def user_params
+      params.require(:user).permit(:email, :password)
     end
 
 end
