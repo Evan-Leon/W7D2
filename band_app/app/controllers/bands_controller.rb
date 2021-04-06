c27b7589a229ce671ce309b6622678aef49519d9
Evan-Leon <evan.leon.j@gmail.com>
2021-04-06 17:33:46 -0400

    added several bands RESTful routes and associated views

--- band_app/app/controllers/bands_controller.rb
+++ band_app/app/controllers/bands_controller.rb
@@ -7,4 +7,32 @@ class BandsController < ApplicationController
       render :index 
     end
 
+    def show 
+      @band = Band.find(params[:id])
+
+      render :show 
+    end
+
+    def create 
+        @band = Band.new(band_params)
+
+        if @band.save 
+            redirect_to band_url(@band)
+        else  
+            flash[:errors] = @band.erros.full_message 
+            redner :new 
+        end
+    end
+
+    def new 
+        @band = Band.new 
+
+        render :new 
+    end
+
+
+    def band_params
+        params.require(:band).permit(:name)
+    end
+
 end
